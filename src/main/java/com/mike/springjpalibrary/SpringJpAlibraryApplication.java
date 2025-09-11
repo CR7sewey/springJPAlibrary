12de6237573c7c9a103b5d13f7d22e797fba6338
Miguel Mota Carvalho <miguel.mota.carvalho@criticalsoftware.com>
2025-09-11 13:36:04 +0100

    Initial commit

---
+++ src/main/java/com/mike/springjpalibrary/SpringJpAlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.mike.springjpalibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpAlibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpAlibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mike/springjpalibrary/SpringJpAlibraryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mike.springjpalibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringJpAlibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
