76f087c73a1ff557574f3ed043737cf62438e791
Miguel Mota Carvalho <miguel.mota.carvalho@criticalsoftware.com>
2025-09-12 15:31:03 +0100

    Repository + Tests

--- src/main/java/com/mike/springjpalibrary/repository/BookRepository.java
+++ src/main/java/com/mike/springjpalibrary/repository/BookRepository.java
@@ -1,15 +1,10 @@
 package com.mike.springjpalibrary.repository;
 
 import com.mike.springjpalibrary.model.Author;
+import com.mike.springjpalibrary.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-
-public interface AuthorRepository extends JpaRepository<Author, UUID> {
-    Author findByNome(String nome);
-
-
+public interface BookRepository extends JpaRepository<Book, UUID> {
 }
-
-
---
+++ src/test/java/com/mike/springjpalibrary/repository/AuthorRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.mike.springjpalibrary.repository;
+
+import com.mike.springjpalibrary.model.Author;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@SpringBootTest
+public class AuthorRepositoryTest {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Test
+    public void saveAuthor() {
+        Author author = new Author();
+        author.setNome("MiguelDelete");
+        author.setBirthDate(LocalDate.of(1980, 1, 1));
+        author.setNationality("Portuguese");
+        var author1 = authorRepository.save(author);
+        System.out.println(author1);
+    }
+
+    @Test
+    public void updateAuthor() {
+        var id = UUID.fromString("5ce335ec-0744-4ada-8092-fb127374ac48");
+
+        var author = authorRepository.findById(id);
+        if (author.isPresent()) {
+
+            author.get().setId(id);
+            author.get().setNome("Miguel 2");
+            var author1 = authorRepository.save(author.get());
+            System.out.println(author1);
+        }
+        else  {
+            System.out.println("Author not found");
+        }
+    }
+
+    @Test
+    public void deleteAuthor() {
+
+        var author = authorRepository.findByNome("MiguelDelete");
+        if (author != null) {
+            authorRepository.delete(author);
+        }
+        else  {
+            System.out.println("Author not found");
+        }
+    }
+}
